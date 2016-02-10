c362113477e2aeaa2a2e6954becb9c3a468d7d2a
ozcarzarate <oscar.zarate@gmail.com>
2016-02-10 23:00:14 +1100

    S

---
+++ Investment.Domain/Distribution.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Investment.Domain
+{
+    public class Distribution
+    {
+        public string Code { get; set; }
+        public DateTime DateOfDistribution { get; set; }
+        public double CentPerShare { get; set; }
+    }
+}
---
+++ Investment.Domain/Investment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Investment.Domain
+{
+    public class Investment
+    {
+        
+        public double CommissionPaidWhenBought { get; set; }
+        public double CurrentPrice { get; set; }
+        public double TotalDistributionSinceBought { get; set; }
+        public double ComissionPaidWhenSell { get; set; }
+    }
+}
