c2f4d8b99a1141170430606f75ad07b8ae62d5f4
ozcarzarate <oscar.zarate@gmail.com>
2016-02-14 21:42:40 +1100

    Added a Script Generator for Code First Migrations

---
+++ Investment.Persistance.Console.Generator/Program.cs
@@ -0,0 +1,14 @@
+﻿using Investment.Persistance.Migrations;
+
+namespace Investment.Persistance.Console.Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = new ScriptGenerator();
+            var result = generator.Run(null, null);
+            System.Console.WriteLine(result);
+}
+    }
+}
---
+++ Investment.Persistance/Migrations/ScriptGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity.Migrations;
+using System.Data.Entity.Migrations.Infrastructure;
+
+namespace Investment.Persistance.Migrations
+{
+    public class ScriptGenerator
+    {
+        public string Run(string from, string to)
+        {
+            var migrator = new DbMigrator(new Configuration());
+            var scriptor = new MigratorScriptingDecorator(migrator);
+            var sql = scriptor.ScriptUpdate("", "a");
+            //var sql = scriptor.ScriptUpdate("Name-Of-Source-Migration", "Name-Of-Target-Migration");
+            return sql;
+        }
+    }
+}
